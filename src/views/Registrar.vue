ba20afeafca8d03a31a4cd72376ba3b702ad26c7
Jonathan Moreno <jmoreno@gruposcanner.com>
2022-01-11 16:14:37 -0500

    Preparado para hacer peticiones

---
+++ src/views/Registrar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-tabs vertical>
+      <b-tab-item label="prueba"></b-tab-item>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clientes: [],
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    init(){
+      
+    }
+  },
+};
+</script>
